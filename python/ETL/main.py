89fb3ce757d9644d22021cd1190c60ecf416edb0
Christopher Chalcraft <chris.chalcraft@gmail.com>
2023-08-01 01:06:21 -0700

    update

--- python/ETL/main.py
+++ python/ETL/main.py
@@ -6,13 +6,13 @@ import transformations as trans
 
 
 def main():
-  c = Config(profile="PROFILE", cluster_id="CLUSTER")
+  c = Config(profile="DEFAULT", cluster_id="0801-073038-t64kumcx")
   spark = SparkSession.builder.sdkConfig(c).getOrCreate()
   print(spark.range(100).collect())
 
   # Step 1 - Load the Bronze Tables
   print("Step 1 .....")
-  table_name_bronze = "main.martingrund.taxi_demo_bronze"
+  table_name_bronze = "main.cjc.taxi_demo_bronze"
   df = trans.load_data(spark)
   trans.save_data(df, table_name_bronze)
 
@@ -23,15 +23,15 @@ def main():
   df = trans.transform_columns(df)
   df = trans.filter_invalid(df)
 
-  table_name_silver = "main.martingrund.taxi_demo_silver"
+  table_name_silver = "main.cjc.taxi_demo_silver"
   trans.save_data(df, table_name_silver)
 
   print(spark.table(table_name_silver).count())
 
   # Step 3 Prepare the views
   print("Step 3 .....")
-  yellow_view = "main.martingrund.taxi_demo_view_yellow"
-  green_view = "main.martingrund.taxi_demo_view_green"
+  yellow_view = "main.cjc.taxi_demo_view_yellow"
+  green_view = "main.cjc.taxi_demo_view_green"
   trans.create_view(spark, table_name_silver, yellow_view, 1)
   trans.create_view(spark, table_name_silver, green_view, 2)
 
